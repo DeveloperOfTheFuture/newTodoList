8480b6126d88f37de7e51fd35c80c5172656a8dd
ne ponyal <kretov.il@mail.ru>
2020-02-18 20:20:19 +0300

    Добавил возможность смены темы

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// Форма
+// Список задач
 const tasks = [{
     _id: '5d2ca9e2e03d40b326596aa7',
     completed: true,
@@ -30,18 +32,93 @@ const tasks = [{
     return acc;
   }, {});
 
-  const listContainer = document.querySelector('.tasks-list-section .list-group');
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
+    },
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+
+  let lastSelectedTheme = 'default';
+
+
+
+  // Elemnts UI
+  const listContainer = document.querySelector(
+    '.tasks-list-section .list-group'
+  );
   const form = document.forms['addTask'];
   const inputTitle = form.elements['title'];
   const inputBody = form.elements['body'];
+  const themeSelect = document.getElementById('themeSelect');
 
+  // Events
   renderAllTasks(objOfTasks);
   form.addEventListener('submit', onFormSubmitHandler);
-  listContainer.addEventListener('click', onDeleteHandler);
+  listContainer.addEventListener('click', onDeletehandler);
+  themeSelect.addEventListener('change', onThemeSelectHandler);
 
   function renderAllTasks(tasksList) {
     if (!tasksList) {
-      console.error('Не передан список задач');
+      console.error('Передайте список задач!');
       return;
     }
 
@@ -59,7 +136,13 @@ const tasks = [{
     body
   } = {}) {
     const li = document.createElement('li');
-    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'align-items-center',
+      'flex-wrap',
+      'mt-2'
+    );
     li.setAttribute('data-task-id', _id);
 
     const span = document.createElement('span');
@@ -85,7 +168,6 @@ const tasks = [{
     e.preventDefault();
     const titleValue = inputTitle.value;
     const bodyValue = inputBody.value;
-    console.log(titleValue, bodyValue);
 
     if (!titleValue || !bodyValue) {
       alert('Введите title и body');
@@ -103,7 +185,7 @@ const tasks = [{
       title,
       body,
       completed: false,
-      _id: `task-${Math.random()}`
+      _id: `task-${Math.random()}`,
     };
 
     objOfTasks[newTask._id] = newTask;
@@ -116,8 +198,8 @@ const tasks = [{
   function deleteTask(id) {
     const {
       title
-    } = objOfTasks[id],
-      isConfirm = confirm(`Удалить задачу: ${title}?`);
+    } = objOfTasks[id];
+    const isConfirm = confirm(`Удалить задачу: ${title}?`);
     if (!isConfirm) return isConfirm;
     delete objOfTasks[id];
     return isConfirm;
@@ -128,7 +210,7 @@ const tasks = [{
     el.remove();
   }
 
-  function onDeleteHandler({
+  function onDeletehandler({
     target
   }) {
     if (target.classList.contains('delete-btn')) {
@@ -139,5 +221,22 @@ const tasks = [{
     }
   }
 
+  function onThemeSelectHandler(e) {
+    const selectedTheme = themeSelect.value;
+    const isConfirmed = confirm(`Хотите сменить тему на ${selectedTheme}?`);
+    if (!isConfirmed) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    }
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+  }
+
+  function setTheme(name) {
+    const selectedThemeObj = themes[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    })
+  }
 
 })(tasks);
